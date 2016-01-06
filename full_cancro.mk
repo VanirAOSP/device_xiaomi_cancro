7efeb187496206217493718fda546130c1dc1051
mr-tweaker <aniket.lamba@gmail.com>
2016-01-06 22:39:55 +0530

    Possibly fix cancro : returns

--- full_cancro.mk
+++ full_cancro.mk
@@ -1,5 +1,3 @@
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
@@ -18,11 +16,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := cancro
 TARGET_VENDOR_DEVICE_NAME := MI3W
-
-# Unofficial build id
-TARGET_UNOFFICIAL_BUILD_ID := audahadi
-
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=cancro PRODUCT_NAME=cancro
-
-## Use the latest approved GMS identifiers unless running a signed build
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/cancro/cancro:6.0/MDB08M/5.11.6:userdebug/test-keys PRIVATE_BUILD_DESC="cancro-userdebug 6.0 MDB08M 5.11.6 test-keys"
