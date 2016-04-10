d46617fcbbe839ded128811d15a9e0ef20cf0b6b
DHOMD <DHO@dho.im>
2016-04-09 22:23:16 -0400

    Fix up build with cm kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,16 +43,16 @@ USE_CLANG_PLATFORM_BUILD := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
 # Kernel
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 BOARD_KERNEL_CMDLINE := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
-TARGET_KERNEL_SOURCE := kernel/xiaomi/cancro
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := cancro_custom_defconfig
+BOARD_KERNEL_SEPARATED_DT          := true
+BOARD_KERNEL_BASE                  := 0x00000000
+BOARD_KERNEL_PAGESIZE              := 2048
+BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
+TARGET_KERNEL_SOURCE               := kernel/xiaomi/cancro
+TARGET_KERNEL_ARCH                 := arm
+TARGET_KERNEL_CONFIG               := cyanogen_cancro_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+BOARD_DTBTOOL_ARGS                 := -2
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
