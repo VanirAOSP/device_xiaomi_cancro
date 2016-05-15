7d7a658df364c8369013d786ce7fdf9419e4ed54
mr-tweaker <aniket.lamba@gmail.com>
2016-05-15 19:00:53 +0530

    Permissive all the way!

--- board/kernel.mk
+++ board/kernel.mk
@@ -9,6 +9,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/cancro
 TARGET_KERNEL_CONFIG := cyanogen_cancro_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 BOARD_DTBTOOL_ARGS := -2
