05e36b1a7ed7daa80fb4bf6c3ecaa9b809129e9f
mr-tweaker <aniket.lamba@gmail.com>
2016-06-22 20:16:05 +0530

    Create full_cancro.mk

---
+++ full_cancro.mk
@@ -0,0 +1,13 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from cancro device
+$(call inherit-product, device/xiaomi/cancro/cancro.mk)
+
+PRODUCT_NAME := full_cancro
+PRODUCT_DEVICE := cancro
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := MI Cancro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
